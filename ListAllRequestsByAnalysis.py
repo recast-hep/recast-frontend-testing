776a473385f202cb4bc6643a759608b82498bfdd
Wayne Motycka <wmotycka@cse.unl.edu>
2015-06-16 11:52:20 -0500

    Marked as deprecated
    marked this test as deprecated for demo use since this test does not use PageObject model

--- ListAllRequestsByAnalysis.py
+++ ListAllRequestsByAnalysis.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# DEPRECATED TEST - FOR DEMOSTRATION PURPOSES ONLY
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
